292165e00d93fd00e29cccdb9b4112b918093f42
Anuraj Yandapalli <164500186+yvnsanuraj@users.noreply.github.com>
2024-05-19 22:36:37 +0530

    Added files to the github.

---
+++ Drum Kit Project/index.js
@@ -0,0 +1,139 @@
+
+document.addEventListener('keypress', function (event)
+ {  
+  const key = event.key;
+  switch (key) {
+    case "a":
+      var a = new Audio("./music/music (1).wav");
+      a.play();
+      break;
+
+    case "b":
+      var b = new Audio("./music/music (2).wav");
+      b.play();
+      break;
+
+    case "c":
+      var c = new Audio("./music/music (3).wav");
+      c.play();
+      break;
+
+    case "d":
+      var d = new Audio("./music/music (4).wav");
+      d.play();
+      break;
+
+    case "e":
+      var e = new Audio("./music/music (5).wav");
+      e.play();
+      break;
+
+    case "f":
+      var f = new Audio("./music/music (6).wav");
+      f.play();
+      break;
+
+    case "g":
+      var g = new Audio("./music/music (7).wav");
+      g.play();
+      break;
+
+    case "h":
+      var h = new Audio("./music/music (8).wav");
+      h.play();
+      break;
+
+    case "i":
+      var i = new Audio("./music/music (9).wav");
+      i.play();
+      break;
+
+    case "j":
+      var j = new Audio("./music/music (10).wav");
+      j.play();
+      break;
+
+    case "k":
+      var k = new Audio("./music/music (11).wav");
+      k.play();
+      break;
+
+    case "l":
+      var l = new Audio("./music/music (12).wav");
+      l.play();
+      break;
+
+    case "m":
+      var m = new Audio("./music/music (13).wav");
+      m.play();
+      break;
+
+    case "n":
+      var n = new Audio("./music/music (14).wav");
+      n.play();
+      break;
+
+    case "o":
+      var o = new Audio("./music/music (15).wav");
+      o.play();
+      break;
+
+    case "p":
+      var p = new Audio("./music/music (16).wav");
+      p.play();
+      break;
+
+    case "q":
+      var q = new Audio("./music/music (17).wav");
+      q.play();
+      break;
+
+    case "r":
+      var r = new Audio("./music/music (18).wav");
+      r.play();
+      break;
+
+    case "s":
+      var s = new Audio("./music/music (19).wav");
+      s.play();
+      break;
+
+    case "t":
+      var t = new Audio("./music/music (20).wav");
+      t.play();
+      break;
+
+    case "u":
+      var u = new Audio("./music/music (21).wav");
+      u.play();
+      break;
+
+    case "v":
+      var v = new Audio("./music/music (22).wav");
+      v.play();
+      break;
+
+    case "w":
+      var w = new Audio("./music/music (23).wav");
+      w.play();
+      break;
+
+    case "x":
+      var x = new Audio("./music/music (24).wav");
+      x.play();
+      break;
+
+    case "y":
+      var y = new Audio("./music/music (25).wav");
+      y.play();
+      break;
+
+    case "z":
+      var z = new Audio("./music/music (26).wav");
+      z.play();
+      break;
+
+    default: alert('Press any alphabet');
+  }
+})
+
